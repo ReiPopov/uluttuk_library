b46f27ac7c668f0623e463b460431dc19a99fe01
Andrey <rei_popov@mail.ru>
2023-04-28 00:17:47 +0600

    added unique title for frame

--- src/pages/ContactsPage/ui/MapBlock/MapBlock.js
+++ src/pages/ContactsPage/ui/MapBlock/MapBlock.js
@@ -5,6 +5,7 @@ export const MapBlock = () => {
     <div className={'container'} style={{marginTop: 60}}>
       <Flex justify={'center'}>
         <iframe
+          title="map_frame"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4233.348054308968!2d74.60746156007596!3d42.880032349723244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389eb7ea1fcbecef%3A0x78aaeabf4292fc87!2z0J3QsNGG0LjQvtC90LDQu9GM0L3QsNGPINCR0LjQsdC70LjQvtGC0LXQutCwINCa0YvRgNCz0YvQt9GB0LrQvtC5INCg0LXRgdC_0YPQsdC70LjQutC4INC40LwuINCQLiDQntGB0LzQvtC90L7QstCw!5e0!3m2!1sru!2skg!4v1682617867102!5m2!1sru!2skg"
           width="1000"
           height="450"
